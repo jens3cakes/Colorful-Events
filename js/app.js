d5a40b0f1d277fdb8e996b188df37d8f91ca1bc5
Jennifer Hikichi <jenniferhikichi@Jennifers-MacBook-Pro.local>
2018-09-24 18:09:42 -1000

    change to color not working

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
+function changeColor(){
+   var pickColor = blingNewColor();
+   var getButton = document.getElementById('color').style.backgroundColor = pickColor;
+   
+}
 
+   function blingNewColor(){
+       var empArr = [];
+       for (var i =0; i < 3;i++){
+           var rgb = Math.floor(Math.random() * 255) 
+           empArr.push(rgb);
+           
+       }    
+    return "rgb:(" + empArr[0] + ','+ empArr[1] + ','+ empArr[2]+ ')'
+      };
+//console.log(changeColor())
+//console.log(blingNewColor());
+//console.log(pickColor);
